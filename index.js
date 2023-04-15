5352ef05214024e8b03c7db660cdba3b8df30346
Ruth Toh <ruthtoh@live.com.sg>
2023-04-15 22:23:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const main = async () => {
             'X-GitHub-Api-Version': '2022-11-28'
         }
     })
-    core.info(JSON.stringify(result.tree));
+    core.info(JSON.stringify(result));
     const { data: changedFiles } = await octokit.rest.pulls.listFiles({
         owner,
         repo,
