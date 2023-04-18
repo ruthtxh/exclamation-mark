3895220bfc745df313e718106ae5d68cf7a3373a
Ruth Toh <ruthtoh@live.com.sg>
2023-04-19 00:38:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,75 +24,72 @@ const main = async () => {
 
 
     const tree = result.data.tree;
-    const mdFileArr = [];
+    // const mdFileArr = [];
 
-    const populateMdFileArr = new Promise((resolve, reject) => {
-        tree.forEach(async (element, index, array) => {
-            // get content of markdown files
-            const path = element.path;
-            const fileType = path.split('.').pop();
-            if (fileType.toLowerCase() === "md") {
-                const file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-                    owner: owner,
-                    repo: repo,
-                    path: path,
-                    headers: {
-                        'X-GitHub-Api-Version': '2022-11-28'
-                    }
-                });
-                const content = Buffer.from(file.data.content, 'base64').toString('utf8');
-                // get index of markdown images that do not contain alt text
-                const regex = /!\[\]\(/gi;
-                let result, indices = [];
-                while ((result = regex.exec(content))) {
-                    indices.push(result.index);
+    const promises = tree.map(async (element) => {
+        const path = element.path;
+        const fileType = path.split('.').pop();
+        if (fileType.toLowerCase() === "md") {
+            const file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+                owner: owner,
+                repo: repo,
+                path: path,
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
                 }
-                if (indices.length > 0) {
-                    // push to array url
-                    let mdFile = { path: element.path, urlArr: [] }
-                    for (let i = 0; i < indices.length; i++) {
-                        mdFile.urlArr.push(content.substring(indices[i] + 4, indices[i + 1]).split(")")[0]);
-                    }
-                    mdFileArr.push(mdFile);
+            });
+            const content = Buffer.from(file.data.content, 'base64').toString('utf8');
+            // get index of markdown images that do not contain alt text
+            const regex = /!\[\]\(/gi;
+            let result, indices = [];
+            while ((result = regex.exec(content))) {
+                indices.push(result.index);
+            }
+            if (indices.length > 0) {
+                // push to array url
+                let mdFile = { path: element.path, urlArr: [] }
+                for (let i = 0; i < indices.length; i++) {
+                    return (content.substring(indices[i] + 4, indices[i + 1]).split(")")[0]);
                 }
             }
-            if (index === array.length - 1) resolve();
-        });
+        }
     });
 
+    const mdFileArr = await Promise.all(promises);
+    console.log(mdFileArr);
 
-    populateMdFileArr.then(async () => {
-        await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
-            owner: owner,
-            repo: repo,
-            name: 'Markdown image alt text checker',
-            head_sha: sha,
-            status: 'completed',
-            conclusion: 'failure',
-            output: {
-                title: 'Markdown image missing alt text',
-                summary: 'Add alt text to image',
-                text: '',
-                annotations: [
-                    {
-                        path: mdFileArr[0].path,
-                        start_line: 2,
-                        end_line: 4,
-                        annotation_level: 'failure',
-                        message: 'Markdown image missing alt text',
-                        start_column: 3,
-                        end_column: 4
-                    }
-                ]
-            },
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        })
+    // populateMdFileArr.then(async () => {
+    //     await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
+    //         owner: owner,
+    //         repo: repo,
+    //         name: 'Markdown image alt text checker',
+    //         head_sha: sha,
+    //         status: 'completed',
+    //         conclusion: 'failure',
+    //         output: {
+    //             title: 'Markdown image missing alt text',
+    //             summary: 'Add alt text to image',
+    //             text: '',
+    //             annotations: [
+    //                 {
+    //                     path: mdFileArr[0].path,
+    //                     start_line: 2,
+    //                     end_line: 4,
+    //                     annotation_level: 'failure',
+    //                     message: 'Markdown image missing alt text',
+    //                     start_column: 3,
+    //                     end_column: 4
+    //                 }
+    //             ]
+    //         },
+    //         headers: {
+    //             'X-GitHub-Api-Version': '2022-11-28'
+    //         }
+    //     })
 
-    }).catch(() => {
-        console.log("Promise Rejected");
-    });;
+    // }).catch(() => {
+    //     console.log("Promise Rejected");
+    // });;
 
     // azure.computerVision(key, endpoint).then((suggestedText) => { console.log(suggestedText) })
     // core.info(altText)
